01542171076f11187c48545cd7b01b5e56039046
Justin <justinj.lambeth@gmail.com>
2020-05-16 23:51:35 -0600

    Having issues configuring the request and item update, I have a few ideas but need more time to go through them'

--- bamazon.sql
+++ bamazon.sql
@@ -2,10 +2,16 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-item_id INT NOT NULL AUTO_INCREMENT,
+item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(255), 
 department_name VARCHAR(255),
 price DECIMAL(10,2),
 stock_quantity INT,
 PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("GPU", "Computers", 299.99, 6), ("CPU", "Computers", 99.89, 25), ("Motherboard", "Computers", 79.99, 12), ("Memory card", "Computers", 50.00, 10), 
+("Keyboard", "Computers", 149.99, 15), ("HD Monitor", "Computers", 289.99, 3), ("Headset", "Audio", 19.99, 10), ("Skittles", "Pets", 0.99, 1);
+
+SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,3 +39,83 @@ Once the update goes through, show the customer the total cost of their purchase
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: "root",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if(err) {
+      console.error("Error Connection: ", err);
+    }
+    loadProducts();
+  });
+  
+  function loadProducts() {
+    const query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products";
+    connection.query(query, (err, res) => {
+      if(err) throw err;
+      console.log("\n\n===============================================================================================\n");
+      console.table(res);
+      console.log("===============================================================================================\n\n");
+      questionItem(res);
+    })
+  }
+  
+  function questionItem(products) {
+    inquirer.prompt({
+      type: "input",
+      name: "item",
+      message: "What is the ID of the item you would like to purchase?"
+    }).then(answer => {
+      const choice = parseInt(answer.item)
+      const product = checkInventory(choice, products);
+  
+      if(product) {
+        questionQuantity(product)
+      } else {
+        console.log("\nThat item does not exist in our inventory");
+        loadProducts();
+      }
+    })
+  }
+  
+  function questionQuantity(product) {
+   inquirer.prompt({
+     type: "input",
+     name: "quantity",
+     message: "How many would you like to purchase?"
+   }).then(answer => {
+     const quantity = parseInt(answer.quantity);
+  
+     if(quantity > product.stock_quantity) {
+       console.log("\nNot enough in stock\n");
+       loadProducts();
+     } else {
+       purchase(product, quantity);
+     }
+   })
+  }
+  
+  function checkInventory(choice, products) {
+    for(let i = 0; i < products.length; i++) {
+      const product = products[i];
+      if(product.item_id === choice) {
+        return product;
+      }
+    }
+    return null;
+  }
+  
+  function purchase(product, quantity) {
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ?, product_sold = product_sold + ? WHERE item_id = ?",
+        [quantity, product.price * quantity, product.item_id],
+        (err, res) => {
+          if(err) throw err;
+          console.log("\nYou purchased " + quantity + " " + product.product_name);
+          loadProducts();
+        });
+  }
